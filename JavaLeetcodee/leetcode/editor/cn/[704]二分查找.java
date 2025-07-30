308a58390b100bc46add4e09af087bfd0a378fa5
yy <1667253917@qq.com>
2025-07-30 12:25:49 +0800

    day1: 数组-二分查找

--- JavaLeetcodee/leetcode/editor/cn/[704]二分查找.java
+++ JavaLeetcodee/leetcode/editor/cn/[704]二分查找.java
@@ -37,10 +37,8 @@ class Solution {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length;
-
         while(left < right){
             int mid = (left + right) / 2;
-
             if(nums[mid] > target){
                 right = mid;
             }else if(nums[mid] < target){
@@ -49,7 +47,6 @@ class Solution {
                 return mid;
             }
         }
-
         return -1;
     }
 }
