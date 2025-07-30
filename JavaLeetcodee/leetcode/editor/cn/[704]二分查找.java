0472d593f82f106a398701ea1fbfdbf9b5cb32dd
yy <1667253917@qq.com>
2025-07-30 12:19:34 +0800

    day1: 数组-二分查找

--- JavaLeetcodee/leetcode/editor/cn/[704]二分查找.java
+++ JavaLeetcodee/leetcode/editor/cn/[704]二分查找.java
@@ -36,12 +36,13 @@
 class Solution {
     public int search(int[] nums, int target) {
         int left = 0;
-        int right = nums.length - 1;
+        int right = nums.length;
 
-        while(left <= right){
-            int mid = (left + right) / 2;
+        while(left < right){
+            mid = (left + right) / 2
+
             if(nums[mid] > target){
-                right = mid - 1; //mid不是需要的值
+                right = mid;
             }else if(nums[mid] < target){
                 left = mid + 1;
             }else{
