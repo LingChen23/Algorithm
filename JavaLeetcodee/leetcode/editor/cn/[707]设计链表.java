7a0340ce8f85a4528dd73695fe32229d3b58add8
yy <1667253917@qq.com>
2025-08-06 22:04:18 +0800

    day6: 链表-设计链表

---
+++ JavaLeetcodee/leetcode/editor/cn/[707]设计链表.java
@@ -0,0 +1,157 @@
+//你可以选择使用单链表或者双链表，设计并实现自己的链表。 
+//
+// 单链表中的节点应该具备两个属性：val 和 next 。val 是当前节点的值，next 是指向下一个节点的指针/引用。 
+//
+// 如果是双向链表，则还需要属性 prev 以指示链表中的上一个节点。假设链表中的所有节点下标从 0 开始。 
+//
+// 实现 MyLinkedList 类： 
+//
+// 
+// MyLinkedList() 初始化 MyLinkedList 对象。 
+// int get(int index) 获取链表中下标为 index 的节点的值。如果下标无效，则返回 -1 。 
+// void addAtHead(int val) 将一个值为 val 的节点插入到链表中第一个元素之前。在插入完成后，新节点会成为链表的第一个节点。 
+// void addAtTail(int val) 将一个值为 val 的节点追加到链表中作为链表的最后一个元素。 
+// void addAtIndex(int index, int val) 将一个值为 val 的节点插入到链表中下标为 index 的节点之前。如果 
+//index 等于链表的长度，那么该节点会被追加到链表的末尾。如果 index 比长度更大，该节点将 不会插入 到链表中。 
+// void deleteAtIndex(int index) 如果下标有效，则删除链表中下标为 index 的节点。 
+// 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入
+//["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", 
+//"deleteAtIndex", "get"]
+//[[], [1], [3], [1, 2], [1], [1], [1]]
+//输出
+//[null, null, null, null, 2, null, 3]
+//
+//解释
+//MyLinkedList myLinkedList = new MyLinkedList();
+//myLinkedList.addAtHead(1);
+//myLinkedList.addAtTail(3);
+//myLinkedList.addAtIndex(1, 2);    // 链表变为 1->2->3
+//myLinkedList.get(1);              // 返回 2
+//myLinkedList.deleteAtIndex(1);    // 现在，链表变为 1->3
+//myLinkedList.get(1);              // 返回 3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= index, val <= 1000 
+// 请不要使用内置的 LinkedList 库。 
+// 调用 get、addAtHead、addAtTail、addAtIndex 和 deleteAtIndex 的次数不超过 2000 。 
+// 
+//
+// Related Topics 设计 链表 👍 1156 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+//单链表
+class MyLinkedList {
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) {
+            this.val=val;
+        }
+    }
+    //size存储链表元素的个数
+    private int size;
+    //注意这里记录的是虚拟头结点
+    private ListNode head;
+
+    //初始化链表
+    public MyLinkedList() {
+        this.size = 0;
+        this.head = new ListNode(0);
+    }
+
+    //获取第index个节点的数值，注意index是从0开始的，第0个节点就是虚拟头结点
+    public int get(int index) {
+        //如果index非法，返回-1
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+        ListNode cur = head;
+        //第0个节点是虚拟头节点，所以查找第 index+1 个节点
+        for (int i = 0; i <= index; i++) {
+            cur = cur.next;
+        }
+        return cur.val;
+    }
+
+    public void addAtHead(int val) {
+        ListNode newNode = new ListNode(val);
+        newNode.next = head.next;
+        head.next = newNode;
+        size++;
+
+        // 在链表最前面插入一个节点，等价于在第0个元素前添加
+        // addAtIndex(0, val);
+    }
+
+
+    public void addAtTail(int val) {
+        ListNode newNode = new ListNode(val);
+        ListNode cur = head;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = newNode;
+        size++;
+
+        // 在链表的最后插入一个节点，等价于在(末尾+1)个元素前添加
+        // addAtIndex(size, val);
+    }
+
+    // 在第 index 个节点之前插入一个新节点，例如index为0，那么新插入的节点为链表的新头节点。
+    // 如果 index 等于链表的长度，则说明是新插入的节点为链表的尾结点
+    // 如果 index 大于链表的长度，则返回空
+    public void addAtIndex(int index, int val) {
+        if (index < 0 || index > size) {
+            return;
+        }
+
+        //找到要插入节点的前驱
+        ListNode pre = head;
+        for (int i = 0; i < index; i++) {
+            pre = pre.next;
+        }
+        ListNode newNode = new ListNode(val);
+        newNode.next = pre.next;
+        pre.next = newNode;
+        size++;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            return;
+        }
+
+        //因为有虚拟头节点，所以不用对index=0的情况进行特殊处理
+        ListNode pre = head;
+        for (int i = 0; i < index ; i++) {
+            pre = pre.next;
+        }
+        pre.next = pre.next.next;
+        size--;
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
